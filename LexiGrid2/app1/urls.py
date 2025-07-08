7e53001a487bfd5e01636d35277637eae18e283d
Kavya-Kulal <kavyashankarkulal@gmai.com>
2025-07-08 12:06:40 +0530

    URL in login page changed

--- LexiGrid2/app1/urls.py
+++ LexiGrid2/app1/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('creator',views.creator,name="creator"),
-     path('user',views.user,name="user"),
+    path('creator/',views.creator,name="creator"),
+     path('user/',views.user,name="user"),
      path('',views.home,name="")
 ]
